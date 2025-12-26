8656d837c4e90c930b31dd58407fd2d22804a5bf
pablomarotta4 <111512542+pablomarotta4@users.noreply.github.com>
2025-12-26 19:11:22 -0300

    [REFACTOR] Improve Flyway integration and update User schema
    - Migrate Flyway configuration from Maven plugin to Spring Boot integration.
    - Update `application.yaml` with new Flyway settings and logging levels.
    - Modify `V1__init.sql` to include new User schema changes.
    - Remove `V2__update_user_schema.sql` and consolidate changes into `V1__init.sql`.
    - Update `UserController` with a create user endpoint using `UserService`.

--- src/main/java/com/pablomarotta/smart_task_manager/UserController.java
+++ src/main/java/com/pablomarotta/smart_task_manager/UserController.java
@@ -1,8 +1,25 @@
 package com.pablomarotta.smart_task_manager;
 
-import org.springframework.stereotype.Controller;
+import com.pablomarotta.smart_task_manager.dto.UserRequest;
+import com.pablomarotta.smart_task_manager.dto.UserResponse;
+import com.pablomarotta.smart_task_manager.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse createUser(@RequestBody UserRequest userRequest) {
+        log.info("Creating user with username: {}", userRequest.getUsername());
+        return userService.createUser(userRequest);
+    }
 }
